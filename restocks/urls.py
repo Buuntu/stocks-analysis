db246b5d0c967907c04058e1dfaf8784d54aa53d
Gabriel Abud <gabriel.jabud@gmail.com>
2019-02-18 09:36:31 -0800

    Fixed image not loading in production

--- restocks/urls.py
+++ restocks/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     url('^', TemplateView.as_view(template_name='index.html'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import './Navbar.css';
 import {IndexLinkContainer, LinkContainer} from 'react-router-bootstrap';
+import logo from '../arrow.svg'
 
 const navbar = () => {
   return (
   <Navbar bg="dark" variant="dark">
   <Nav className="mr-auto">
-    <Navbar.Brand href="/"><img src={"arrow.svg"} id="logo"/>
+    <Navbar.Brand href="/"><img src={logo} id="logo"/>
     Restocks</Navbar.Brand>
     <IndexLinkContainer to="/">
       <Nav.Link>Stocks</Nav.Link>
