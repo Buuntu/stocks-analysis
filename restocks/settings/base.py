ae606b6a4388d04944c933e959fdba7dc461eeab
Gabriel Abud <gabriel.jabud@gmail.com>
2019-02-17 23:49:09 -0800

    Trying to get it to work

--- restocks/settings/base.py
+++ restocks/settings/base.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.realpath(os.path.dirname(__file__) + "/.."))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -25,7 +24,7 @@ SECRET_KEY = 'z6rj7+p8o4-ps0!7#v1c8fj9rr(*4#_0a02b=cwehj$j45o4xf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['strains-analysis.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,6 +124,9 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'build/static')
 ]
