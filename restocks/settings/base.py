d33828dd16d04108f6c8d9337112265ae921c4ba
Gabriel Abud <gabriel.jabud@gmail.com>
2019-02-18 09:01:08 -0800

    Fixed dependency issue

--- restocks/settings/base.py
+++ restocks/settings/base.py
@@ -123,9 +123,10 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'assets')
 ]
